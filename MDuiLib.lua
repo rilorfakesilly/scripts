118a64d527773cf4b34d2fe11bde488611d70f67
rilorfakesilly <rilorfakesilly@gmail.com>
2026-09-25 02:56:48 +0300

    Change font family to Finger-Paint in MDuiLib

--- MDuiLib.lua
+++ MDuiLib.lua
@@ -100,9 +100,9 @@ end
 Library.ActiveGuis = Library.ActiveGuis or {}
 Library.Icons = Library.Icons or {}
 
-local FontMichromaBold = Font.new("rbxasset://fonts/families/Prompt.json", Enum.FontWeight.Heavy, Enum.FontStyle.Normal)
-local FontMichromaRegular = Font.new("rbxasset://fonts/families/Prompt.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal) -- testing how it looks
-local FontMichromaHeavy = Font.new("rbxasset://fonts/families/Prompt.json", Enum.FontWeight.Heavy, Enum.FontStyle.Normal)
+local FontMichromaBold = Font.new("rbxasset://fonts/families/Finger-Paint.json", Enum.FontWeight.Heavy, Enum.FontStyle.Normal)
+local FontMichromaRegular = Font.new("rbxasset://fonts/families/Finger-Paint.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal) -- testing how it looks
+local FontMichromaHeavy = Font.new("rbxasset://fonts/families/Finger-Paint.json", Enum.FontWeight.Heavy, Enum.FontStyle.Normal)
 
 Library.ThemePresets = {
     Dark = {
